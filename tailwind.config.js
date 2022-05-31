f28b96598d530174090260e3da3dad8841956004
mirulnasir <mirul@bestari.io>
2022-06-01 09:43:41 +1000

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   corePlugins: {
     preflight: false,
   },
-  content: ['./sections/*.liquid', './snippets/*.liquid', './layout/*.liquid'],
+  content: [],
   theme: {
     extend: {},
   },
